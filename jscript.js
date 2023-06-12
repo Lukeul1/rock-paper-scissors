df4e107d65769b6a7aaecbecba9478026acb5405
lukeul <lukewillis1@hotmail.com>
2023-06-12 13:11:47 +0100

    Just before loop

--- jscript.js
+++ jscript.js
@@ -5,17 +5,21 @@ function getComputerChoice() {
     return options[(Math.floor(Math.random() * options.length))];
 }
 
-const playerSelection = 'rock';
+const playerSelection = "rock";
 const computerSelection = getComputerChoice();
+
+const result = playRound(playerSelection, computerSelection);
+console.log(result);
 
 function playRound(playerSelection, computerSelection) {
     if (computerSelection === 'paper') {
         return `You win! ${computerSelection} beats ${playerSelection}!`;
-    } else {
+    } else if (computerSelection === 'rock') {
+        return `Tie ${computerSelection} and ${playerSelection} are equivalent!`;
+    }  else { 
         return `You lose! ${playerSelection} beats ${computerSelection}!`;
     }
 }
-console.log(playRound(playerSelection, computerSelection));
 
 function game() {
     
