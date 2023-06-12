c23612ad62a74e5e8801b6d8ac20683b32d4005c
lukeul <lukewillis1@hotmail.com>
2023-06-12 14:04:10 +0100

    Near the end, but still issues with tallying the result

--- jscript.js
+++ jscript.js
@@ -1,26 +1,55 @@
 
 
 function getComputerChoice() {
-    const options = ['rock', 'paper', 'scissors'];
-    return options[(Math.floor(Math.random() * options.length))];
+    const options = ['rock', 'paper', 'scissor'];
+    return options[Math.floor(Math.random() * options.length)];
 }
 
-const playerSelection = "rock";
+const playerSelection = "Rock";
 const computerSelection = getComputerChoice();
 
-const result = playRound(playerSelection, computerSelection);
-console.log(result);
-
 function playRound(playerSelection, computerSelection) {
-    if (computerSelection === 'paper') {
-        return `You win! ${computerSelection} beats ${playerSelection}!`;
-    } else if (computerSelection === 'rock') {
-        return `Tie ${computerSelection} and ${playerSelection} are equivalent!`;
-    }  else { 
-        return `You lose! ${playerSelection} beats ${computerSelection}!`;
+    const playerChoice = playerSelection.toLowerCase();
+    const computerChoice = computerSelection.toLowerCase();
+
+    if (
+        (playerChoice === 'rock' && computerChoice === 'scissor') ||
+        (playerChoice === 'paper' && computerChoice === 'rock') ||
+        (playerChoice === 'scissor' && computerChoice === 'paper')
+    ) {
+        return `You win! ${playerSelection} beats ${computerSelection}!`;
+    } else if (computerChoice === playerChoice) {
+        return `Tie! ${computerSelection} and ${playerSelection} are equivalent!`;
+    } else {
+        return `You lose! ${computerSelection} beats ${playerSelection}!`;
     }
 }
 
-function game() {
-    
+console.log(playRound(playerSelection, computerSelection));
+
+let outcomeTally = {
+    win: 0,
+    loss: 0,
+    tie: 0,
+};
+
+const numRounds = 5;
+
+    for (let i = 0; i < numRounds; i++) {
+        const playerSelection = prompt('Enter your choice:').toLowerCase();
+        const computerSelection = getComputerChoice();
+
+        const outcome = playRound(playerSelection, computerSelection);
+
+        if(outcome.includes('win')) {
+            outcomeTally.win++;
+        } else if (outcome.includes('loss')) {
+            outcomeTally.loss++;
+        } else if (outcome.includes('Tie')) {
+            outcomeTally.tie++;
+        }
+        
+    console.log(outcome);
 }
+
+console.log(outcomeTally);
